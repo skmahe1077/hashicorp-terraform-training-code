f1724fc9519b92bf56fe44cf247681dcdb00bf0f
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2024-12-07 15:34:42 +0000

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "eu-west-1"
+}
+
+resource "aws_instance" "devec2" {
+    ami = "ami-0a422d70f727fe93e"
+    instance_type = "t2.medium"
+}
+
+resource "aws_instance" "testec2" {
+    ami = "ami-04bd4a6a67aa8e86e"
+    instance_type = "t2.medium"
+}
