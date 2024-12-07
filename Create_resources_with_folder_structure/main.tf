0a850451dd2e305a553e075e586e821e58cdc767
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2024-12-07 17:22:30 +0000

    Update main.tf

--- Create_resources_with_folder_structure/main.tf
+++ Create_resources_with_folder_structure/main.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "vijayec2" {
+resource "aws_instance" "devec2" {
     ami = "ami-0a422d70f727fe93e"
     instance_type = "t2.medium"
 }
 
 
-resource "aws_instance" "saranec2" {
+resource "aws_instance" "testec2" {
     ami = "ami-04bd4a6a67aa8e86e"
     instance_type = "t2.medium"
 }
 
 # Create a VPC
-resource "aws_vpc" "vijayvpc" {
+resource "aws_vpc" "devvpc" {
   cidr_block = "10.0.0.0/16"
 
 }
